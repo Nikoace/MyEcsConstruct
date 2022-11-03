7aaaeab94be239d1511ae389b987d2edeed4831a
Kyo <32727925+Nikoace@users.noreply.github.com>
2022-11-03 23:36:16 +0800

    init ecs create cdk code

--- lib/my_ecs_construct-stack.ts
+++ lib/my_ecs_construct-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyEcsConstructStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "MyVpc", {
+      maxAzs: 3
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyEcsConstructQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, "MyCluster", {
+      vpc: vpc
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
+      cluster: cluster,
+      cpu: 256,
+      desiredCount: 6,
+      taskImageOptions: {image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")},
+      memoryLimitMiB: 2048,
+      publicLoadBalancer: true
+    })
   }
 }
